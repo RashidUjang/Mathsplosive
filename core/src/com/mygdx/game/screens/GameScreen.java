554dbcf424dc211471893c2889a885c0c153a0f8
RashidUjang <rashid.uj@gmail.com>
2017-09-22 23:42:19 +0800

    Added sprite roll animation and vertical borders

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -12,21 +12,24 @@ import com.mygdx.game.Mathsplosive;
 public class GameScreen implements Screen {
 	
 	// Speed for movement of image. 40 pixels per second
-	public static float SPEED = 40;
-	public static int SHIP_WIDTH_PIXEL = 17;
-	public static int SHIP_HEIGHT_PIXEL = 32;
+	public static final float SPEED = 300;
+	public static final int SHIP_WIDTH_PIXEL = 17;
+	public static final int SHIP_HEIGHT_PIXEL = 32;
 	// 17 * 3, because the spritesheet for ship is 17
-	public static int SHIP_WIDTH = SHIP_WIDTH_PIXEL * 3;
+	public static final int SHIP_WIDTH = SHIP_WIDTH_PIXEL * 3;
 	// 32 * 3, because the spritesheet for ship is 32
-	public static int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL * 3;
+	public static final int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL * 3;
 	
 	// Animation speed. 0.5 secs per frame
-	public static float SHIP_ANIMATION_SPEED = 0.5f;
+	public static final float SHIP_ANIMATION_SPEED = 0.5f;
 	
+	// 0.15 seconds per switch to next animation
+	public static final float ROLL_TIMER_SWITCH_TIMER = 0.10f;
 	Animation<TextureRegion>[] rolls;
 	
 	float x = 0, y = 0;
 	float stateTime;
+	float rollTimer = 0;
 	
 	// an integer roll to keep track of the roll of the sprite
 	int roll;
@@ -41,9 +44,14 @@ public class GameScreen implements Screen {
 		roll = 2;
 		rolls = new Animation[5];
 		
+		// Splitting the texture by its boxes.
 		TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), SHIP_WIDTH_PIXEL, SHIP_HEIGHT_PIXEL);
 	
-		rolls[roll] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[0]);
+		rolls[0] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[2]); // All left
+		rolls[1] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[1]);
+		rolls[2] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[0]); // No tilt
+		rolls[3] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[3]);
+		rolls[4] = new Animation<TextureRegion>(SHIP_ANIMATION_SPEED, rollSpriteSheet[4]); // All right
 	}
 
 	@Override
@@ -58,6 +66,77 @@ public class GameScreen implements Screen {
 		// if isKeyJustPressed means that is it just recently pressed last frame
 		if (Gdx.input.isKeyPressed(Keys.UP)) {
 			y += SPEED * Gdx.graphics.getDeltaTime();
+		}
+		
+		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			y -= SPEED * Gdx.graphics.getDeltaTime();
+		}
+		
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= SPEED * Gdx.graphics.getDeltaTime();
+			
+			if (x < 0) {
+				x = 0;
+			}
+			
+			// Check if key is JUST pressed (happens during first frame its pressed) and if no right key is being pressed
+			if (Gdx.input.isKeyJustPressed(Keys.LEFT) && !Gdx.input.isKeyPressed(Keys.RIGHT) && roll > 0) {
+				rollTimer = 0;
+				roll--;	
+			}
+			// Minus because going left. 
+			rollTimer -= Gdx.graphics.getDeltaTime();
+			
+			// If 0.15 secs have passed, reset timer and check if its 0 to not roll it too far
+			if(Math.abs(rollTimer) > ROLL_TIMER_SWITCH_TIMER && roll > 0) {
+				rollTimer = 0;
+				roll--;
+			}
+		} else {
+			if (roll < 2) {
+				rollTimer += Gdx.graphics.getDeltaTime();
+				
+				if(Math.abs(rollTimer) > ROLL_TIMER_SWITCH_TIMER && roll < 4) {
+					rollTimer = 0;
+					roll++;
+								
+					if (roll > 4) {
+						roll = 4;
+					}
+				}
+			}
+		}
+		
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += SPEED * Gdx.graphics.getDeltaTime();
+			
+			if (x + SHIP_WIDTH > Gdx.graphics.getWidth()) {
+				x = Gdx.graphics.getWidth() - SHIP_WIDTH;
+			}
+			
+			// Check if key is JUST pressed (happens during first frame its pressed) and if no right key is being pressed
+			if (Gdx.input.isKeyJustPressed(Keys.RIGHT) && !Gdx.input.isKeyPressed(Keys.LEFT) && roll < 4) {
+				rollTimer = 0;
+				roll++;	
+			}
+						
+			// Plus because going right. 
+			rollTimer += Gdx.graphics.getDeltaTime();
+						
+			// If 0.15 secs have passed, reset timer and check if its 0 to not roll it too far
+			if(Math.abs(rollTimer) > ROLL_TIMER_SWITCH_TIMER && roll < 4) {
+				rollTimer = 0;
+				roll++;
+			}
+		} else {
+			if (roll > 2) {
+				rollTimer -= Gdx.graphics.getDeltaTime();
+				
+				if(Math.abs(rollTimer) > ROLL_TIMER_SWITCH_TIMER && roll > 0) {
+					rollTimer = 0;
+					roll--;
+				}
+			}
 		}
 		
 		stateTime += delta;
